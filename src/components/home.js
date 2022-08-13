b152cc1f14644f0b12731f11fc67624f0d90bc77
0x0J <jethrodw@protonmail.com>
2022-08-13 12:13:56 +0200

    Create home.js

---
+++ src/components/home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
+const Home = () => {
+  return (
+      <div className="linkBody">
+        <div className="links">
+          <ul class="">
+          <li className="linkBtn"><NavLink exact to="/mary">PISS MARY</NavLink></li>
+          <li className="linkBtn"><NavLink to="/banksy">NOT BANKSY</NavLink></li>
+          <li className="linkBtn"><NavLink to="/">COMING SOON, NGL</NavLink></li>
+          </ul>
+        </div>
+      </div>
+  );
+};
+
+export default Home;
